407bc6ee91d862e0cda292425bb23764a7aa563e
Andrew Duggan <aduggan@synaptics.com>
2024-10-21 22:08:46 -0700

    Print ID of boot firmware

--- lib/astra_boot_firmware.cpp
+++ lib/astra_boot_firmware.cpp
@@ -18,6 +18,7 @@ int AstraBootFirmware::Load()
         m_productId = std::stoi(manifest["product_id"].as<std::string>(), nullptr, 16);
 
         std::cout << "Loaded boot firmware: " << m_chipName << " " << m_boardName << std::endl;
+        std::cout << "ID: " << m_id << std::endl;
         std::cout << "Secure boot version: " << (m_secureBootVersion == ASTRA_SECURE_BOOT_V2 ? "gen2" : "gen3") << std::endl;
         std::cout << "Vendor ID: 0x" << std::hex << std::uppercase << std::setfill('0') << std::setw(4) << m_vendorId << std::endl;
         std::cout << "Product ID: 0x" << std::hex << std::uppercase << std::setfill('0') << std::setw(4) << m_productId << std::endl;
