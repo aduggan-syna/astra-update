08802f4974cbf5466f4a91a4b7aaee03421e6a1f
Andrew Duggan <aduggan@synaptics.com>
2024-10-25 14:26:50 -0700

    Add status callback

--- lib/astra_device.cpp
+++ lib/astra_device.cpp
@@ -12,11 +12,15 @@ public:
     int Open(std::function<void(AstraDeviceState, int progress, std::string message)> statusCallback) {
         int ret;
         
+        m_statusCallback = statusCallback;
+
         ret = m_device->Open();
         if (ret < 0) {
             std::cerr << "Failed to open device" << std::endl;
             return ret;
         }
+
+        m_statusCallback(ASTRA_DEVICE_STATE_OPENED, 0, "Device opened");
 
         return 0;
     }
@@ -24,6 +28,7 @@ public:
 private:
     std::unique_ptr<USBDevice> m_device;
     AstraDeviceState m_state;
+    std::function<void(AstraDeviceState, int progress, std::string message)> m_statusCallback;
 };
 
 AstraDevice::AstraDevice(std::unique_ptr<USBDevice> device) : 
