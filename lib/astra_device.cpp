ce7685fceaf6dbc7db8d36e1a0a3f9798d670680
Andrew Duggan <aduggan@synaptics.com>
2025-02-26 20:26:07 -0800

    remove htole32 define in astra_device.cpp

--- lib/astra_device.cpp
+++ lib/astra_device.cpp
@@ -6,11 +6,6 @@
 #include <mutex>
 #include <cstring>
 
-#ifdef PLATFORM_MACOS
-#include <libkern/OSByteOrder.h>
-#define htole32(x) OSSwapHostToLittleInt32(x)
-#endif
-
 #include "astra_device.hpp"
 #include "astra_update.hpp"
 #include "astra_boot_firmware.hpp"
